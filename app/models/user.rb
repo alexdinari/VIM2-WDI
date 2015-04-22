f0d319d7f552cd26d70d91d67679050a2dda5962
Your Name <alexdinari@roadhouseapps.com>
2015-04-21 17:44:53 -0700

    Relationships added to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+	has_many :clients, class_name: "Users",
+                          foreign_key: "trainer_id"
+ 
+  	belongs_to :trainers, class_name: "Employee"
 end
