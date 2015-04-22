ce9b1f33c0d2f101c28f6bd1e47dacdba906fb65
Your Name <alexdinari@roadhouseapps.com>
2015-04-21 18:19:52 -0700

    Bundle install for Bootstrap

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
 	has_many :clients, class_name: "Users",
                           foreign_key: "trainer_id"
  
-  	belongs_to :trainers, class_name: "Employee"
+  	belongs_to :trainers, class_name: ""
 end
